aed9667e841495281c2dfaa42a9b17ed8874b23f
Andrey Gorbachev <andreygorbachev@hotmail.com>
2025-01-12 21:03:23 +0000

    Check that we can compile with Boost.

--- quantlib-calendar/quantlib-calendar.cpp
+++ quantlib-calendar/quantlib-calendar.cpp
@@ -20,6 +20,8 @@
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 // SOFTWARE.
 
+#include <boost/math/ccmath/abs.hpp> // test Boost
+
 #include <chrono>
 #include <string>
 #include <iostream>
